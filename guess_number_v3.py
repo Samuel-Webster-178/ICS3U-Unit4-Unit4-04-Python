e96f3f8c9fb93e23f64d3edc177a425a3bfebd36
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-04-20 18:16:33 +0000

    @init@

--- guess_number_v3.py
+++ guess_number_v3.py
@@ -12,20 +12,23 @@ def main():
     # I check if the user guessed correctly
     chosen_number = random.randint(0, 9)
 
-    # input
-    string_guess = input("Guess a number between 0-9: ")
+    while True:
+        # input
+        string_guess = input("Guess a number between 0-9: ")
 
-    # process&output
-    try:
-        int_guess = int(string_guess)
-        if chosen_number == int_guess:
-            print("You got it!")
-        else:
-            print("Maybe next time!")
-    except Exception:
-        print("Please enter an integer!")
-    finally:
-        print("\nDone.")
+        # process&output
+        try:
+            int_guess = int(string_guess)
+            if chosen_number == int_guess:
+                print("You got it!")
+                break
+            elif chosen_number > int_guess:
+                print("Too low!")
+            else:
+                print("Too high!")
+        except Exception:
+            print("Please enter an integer!")
+    print("\nDone.")
 
 
 if __name__ == "__main__":
