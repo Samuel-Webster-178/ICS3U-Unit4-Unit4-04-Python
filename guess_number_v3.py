cac304bf40a7b769455a5fa7f98836b9f23f4142
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-04-20 18:29:38 +0000

    idfk

--- guess_number_v3.py
+++ guess_number_v3.py
@@ -11,16 +11,18 @@ import random
 def main():
     # I check if the user guessed correctly
     chosen_number = random.randint(0, 9)
+    i = 0
 
     while True:
         # input
         string_guess = input("Guess a number between 0-9: ")
+        i += 1
 
         # process&output
         try:
             int_guess = int(string_guess)
             if chosen_number == int_guess:
-                print("You got it!")
+                print("You got it in", i, "tries!")
                 break
             elif chosen_number > int_guess:
                 print("Too low!")
